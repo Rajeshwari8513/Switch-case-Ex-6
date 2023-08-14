ede25e881244766bbef5a6ca28c1022255801323
Rajeshwari8513 <105580174+Rajeshwari8513@users.noreply.github.com>
2023-08-14 12:27:34 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Switch-case-Ex-6
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/js-rvhjag)
---
+++ index.js
@@ -0,0 +1,43 @@
+const month = 2;
+let daysInMonth;
+
+switch (month){
+    case 1 :
+        daysInMonth = 31
+        break;
+    case 2 :
+        daysInMonth = 28
+        break;
+    case 3 :
+        daysInMonth = 31
+        break;
+    case 4 :
+        daysInMonth = 30
+        break;
+    case 5 :
+        daysInMonth = 31
+        break;
+    case 6 :
+        daysInMonth = 30
+        break;
+    case 7 :
+        daysInMonth = 31
+        break;
+    case 8 :
+        daysInMonth = 31
+        break;
+    case 9 :
+        daysInMonth = 30
+        break;
+    case 10 :
+        daysInMonth = 31
+        break;
+    case 11 :
+        daysInMonth = 30
+        break;
+    case 12 :
+        daysInMonth = 31
+        break;
+        
+}
+console.log(`There are ${daysInMonth} days in this month.`);
